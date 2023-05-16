3a41d265dcec4d0ec2da52db6c43e1b0e88d9ec5
gisung.gyeon <gisung.gyeon@gmail.com>
2023-05-16 16:37:23 +0900

    test: header.js,test.js 삭제 및 이미지 추가

--- js/test.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let gnblist = document.querySelectorAll(".gnb_list > li");
-  let catebox = document.querySelector(".cate-box");
-
-  gnblist.addEventListener("mouseover", function (e) {
-    console.log(e.target);
-  });
-});
